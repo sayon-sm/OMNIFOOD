d141b09e2762dc4411243e7112a155ba132c4c46
sm <its.sayon.mitra@gmail.com>
2025-10-27 23:00:52 +0530

    implementing sticky navigation

--- js/script.js
+++ js/script.js
@@ -39,6 +39,28 @@ allLinks.forEach(function (link) {
 });
 
 ///////////////////////////////////////////////////////////
+//STICKY NAVIGATION
+const sectionHeroEl = document.querySelector('.section-hero');
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add('sticky');
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove('sticky');
+    }
+  },
+  {
+    root: null, // in the viewport
+    threshold: 0,
+    rootMargin: '-80px',
+  }
+);
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement('div');
